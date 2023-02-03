04525f41e905b51db288cfce366f7856496df877
VladLeus <vladyslavleus@gmail.com>
2023-02-03 18:49:43 +0200

    Finished project.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ createApp({
     data() {
         return {
             isDarked: false,
+            isShown: true,
+            isFilled: false,
+            notes: [],
+            headingValue: '',
+            noteValue: '',
         }
     },
     methods: {
@@ -17,6 +22,34 @@ createApp({
                 document.querySelector('html').classList.remove('dark');
                 this.isDarked = false;
             }
+        },
+        headingChangeHandler(event) {
+            this.headingValue = event.target.value;
+        },
+        noteChangeHandler(event) {
+            this.noteValue = event.target.value;
+        },
+        addNote() {
+            if (this.headingValue !== '' && this.noteValue !== '') {
+                this.notes.push([this.headingValue, this.noteValue]);
+                this.headingValue = '';
+                this.noteValue = '';
+                this.isFilled = false;
+                console.log(`isFilled: ${this.isFilled}`);
+            } else {
+                this.isFilled = true;
+                console.log(`isFilled: ${this.isFilled}`);
+            }
+        },
+        showDeleteBtn(index) {
+            if (document.getElementById(`${index}`).classList.contains('hidden')){
+                document.getElementById(`${index}`).classList.remove('hidden');
+            } else {
+                document.getElementById(`${index}`).classList.add('hidden')
+            }
+        },
+        deleteNote(index) {
+            this.notes.splice(index,1);
         }
     }
 }).mount('#app')
